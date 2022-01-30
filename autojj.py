11242e6cf8352585822c688a1f612067f5a173e2
fhb04 <84425290+fhb04@users.noreply.github.com>
2022-01-29 22:42:47 -0500

    Update autojj.py

--- autojj.py
+++ autojj.py
@@ -3,29 +3,20 @@ import pydirectinput
 from pynput.keyboard import Controller
 import time
 
-
 keyboard = Controller()
 
 print(f'You have 5 seconds to get to your input window.')
 time.sleep(5)
 
-
-for i in range(1, 101):
+for i in range(1, 101):  # The number of JJs being done.
     pydirectinput.keyDown('/')
     pydirectinput.keyUp('/')
 
     keyboard.type(num2words.num2words(i).upper()+'!')
-    time.sleep(0.3)
+    time.sleep(0.3)  # Gives the program time to actually write the number.
 
     pydirectinput.keyDown('enter')
     pydirectinput.keyUp('enter')
     pydirectinput.keyDown('space')
     pydirectinput.keyUp('space')
-    time.sleep(1.4)
-
-
-
-
-
-
-
+    time.sleep(1.7)  # This is the delay between JJs. Keep it above a second that way it's not obvious you're using a script
